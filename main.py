21ed649ff5b6be6a1ef826c3a73dea150e0122a0
James Kairos <jamesdkairos@gmail.com>
2026-02-09 22:54:27 -0600

    feat: setup messages to expand context later
    feat/refactor: add --verbose flag to show metadata (user prompt, token
    usage)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from google.genai.client import Client
 from google.genai.types import GenerateContentResponse
 
@@ -14,12 +15,16 @@ def main():
 
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="ManicBot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args: argparse.Namespace = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
 
     client: Client = genai.Client(api_key=api_key)
     response: GenerateContentResponse = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        model="gemini-2.5-flash", contents=messages
     )
 
     if not response.usage_metadata:
@@ -27,8 +32,10 @@ def main():
     prompt_tokens: int | None = response.usage_metadata.prompt_token_count
     response_tokens: int | None = response.usage_metadata.candidates_token_count
 
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
     print("Response:")
     print(response.text)
 
