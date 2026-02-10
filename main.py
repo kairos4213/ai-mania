75cd22adc869660216fef8568e6bc33df5613575
James Kairos <jamesdkairos@gmail.com>
2026-02-09 22:06:05 -0600

    feat: added token consumption logging
    style: added typing

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai.client import Client
+from google.genai.types import GenerateContentResponse
 
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key: str | None = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("Api key not found")
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
+    client: Client = genai.Client(api_key=api_key)
+    response: GenerateContentResponse = client.models.generate_content(
         model="gemini-2.5-flash",
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
 
+    if not response.usage_metadata:
+        raise RuntimeError("Failed to generate content response")
+    prompt_tokens: int | None = response.usage_metadata.prompt_token_count
+    response_tokens: int | None = response.usage_metadata.candidates_token_count
+
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print("Response:")
     print(response.text)
 
 
