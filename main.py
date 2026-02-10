72d9018797a45b06d4c99e37573d0a3125967be9
James Kairos <jamesdkairos@gmail.com>
2026-02-09 22:38:35 -0600

    feat: accept user input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai.client import Client
@@ -11,10 +12,14 @@ def main():
     if not api_key:
         raise RuntimeError("Api key not found")
 
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="ManicBot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args: argparse.Namespace = parser.parse_args()
+
     client: Client = genai.Client(api_key=api_key)
     response: GenerateContentResponse = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt,
     )
 
     if not response.usage_metadata:
