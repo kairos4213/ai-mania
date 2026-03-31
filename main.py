6af4474c356e4d7294284a760edc658c51ba8ff8
James Kairos <jamesdkairos@gmail.com>
2026-03-31 13:27:38 -0500

    feat: agent can now call available functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 
 def main():
@@ -42,8 +42,24 @@ def main():
         print(f"Response tokens: {response_tokens}")
     print("Response:")
     if response.function_calls is not None:
+        function_results = []
         for function_call in response.function_calls:
+            function_call_result = call_function(function_call)
+            if (
+                function_call_result.parts is None
+                or len(function_call_result.parts) == 0
+            ):
+                raise Exception("function_call_result.parts is empty")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("parts 1 function_response is None")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("parts 1 function_response.response is None")
+            function_results.append(
+                function_call_result.parts[0].function_response.response
+            )
             print(f"Calling function: {function_call.name}({function_call.args})")
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(response.text)
 
